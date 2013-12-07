bd7e075df284af6efe5bab0efb32e74c4bedaaf2
Franco Pellegrini <frapell@gmail.com>
2013-12-08 00:32:20 +0100

    Moved the interface to the interfaces module. Added validation to the control panel when saving, so we make sure we only store valid Twilio credentials and a valid usable phone number

--- src/collective/twofactor/controlpanel.py
+++ src/collective/twofactor/controlpanel.py
@@ -1,38 +1,77 @@
 # -*- coding: utf-8 -*-
 
 from collective.twofactor import _
+from collective.twofactor.interfaces import ITwilioSettings
+from Products.statusmessages.interfaces import IStatusMessage
 from plone.app.registry.browser import controlpanel
-from zope import schema
+from twilio import TwilioRestException
+from twilio.rest import TwilioRestClient
+from z3c.form import button
 from zope.interface import Interface
-
-
-class ITwilioSettings(Interface):
-    """ Interface for the control panel form.
-    """
-
-    account_sid = schema.TextLine(
-        title=_(u"Account SID"),
-        description=_(u"Your Twilio Account SID."),
-        required=True,
-    )
-
-    auth_token = schema.TextLine(
-        title=_(u"Auth Token"),
-        description=_(u"Your Twilio Auth Token."),
-        required=True,
-    )
-
-    phone_number = schema.TextLine(
-        title=_(u"Phone Number"),
-        description=_(u"Your Twilio phone number from where to send SMS."),
-        required=True,
-    )
 
 
 class TwilioSettingsEditForm(controlpanel.RegistryEditForm):
     schema = ITwilioSettings
     label = _(u'Twilio Settings')
     description = _(u'Settings for the Twilio SMS service')
+    
+    def validate_twilio_account(self, data):
+        failure = False
+        valid_phone = False
+
+        client = TwilioRestClient(data['account_sid'], data['auth_token'])
+        try:
+            numbers = client.phone_numbers.list()
+        except TwilioRestException as e:
+            if e.status == 401:
+                IStatusMessage(self.request).addStatusMessage(
+                    _(u"The SID or token are invalid and we couldn't "
+                       "authenticate with Twilio, please try again."),
+                    "error")
+            failure = True
+                
+        if not failure:
+            for number in numbers:
+                if number.phone_number == data['phone_number']:
+                    valid_phone = True
+                    break
+
+            if not valid_phone:
+                IStatusMessage(self.request).addStatusMessage(
+                    _(u"The phone number you entered doesn't seem valid for "
+                       "this Twilio account, please make sure you are "
+                       "entering correctly."),
+                    "error")
+            else:
+                if not number.capabilities.get('sms', False):
+                    IStatusMessage(self.request).addStatusMessage(
+                        _(u"The phone number entered is a valid Twilio number "
+                          "for this account, however, this phone number "
+                          "cannot send SMS. Please make sure to buy a phone "
+                          "number which allows SMS to be sent."),
+                        "error")
+                    valid_phone = False
+
+        return failure, valid_phone
+
+    @button.buttonAndHandler(_('Save'), name='save')
+    def handleSave(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        
+        failure, valid_phone = self.validate_twilio_account(data)
+
+        if not failure and valid_phone:
+            changes = self.applyChanges(data)
+            IStatusMessage(self.request).addStatusMessage(_(u"Changes saved"), "info")
+            self.request.response.redirect("%s/%s" % (self.context.absolute_url(), self.control_panel_view))
+
+    @button.buttonAndHandler(_('Cancel'), name='cancel')
+    def handleCancel(self, action):
+        IStatusMessage(self.request).addStatusMessage(_(u"Edit cancelled"), "info")
+        self.request.response.redirect("%s/%s" % (self.context.absolute_url(), self.control_panel_view))
 
 
 class TwilioSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
--- src/collective/twofactor/interfaces.py
+++ src/collective/twofactor/interfaces.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from collective.twofactor import _
 from zope.interface import Interface
+from zope import schema
+
+
+class ITwilioSettings(Interface):
+    """ Interface for the control panel form.
+    """
+
+    account_sid = schema.TextLine(
+        title=_(u"Account SID"),
+        description=_(u"Your Twilio Account SID."),
+        required=True,
+    )
+
+    auth_token = schema.Password(
+        title=_(u"Auth Token"),
+        description=_(u"Your Twilio Auth Token."),
+        required=True,
+    )
+
+    phone_number = schema.TextLine(
+        title=_(u"Phone Number"),
+        description=_(u"Your Twilio phone number from where to send SMS. "
+                       "International format, with no special characters. "
+                       "Example: +15555555555"),
+        required=True,
+    )
 
 
 class ITwoFactorLayer(Interface):
