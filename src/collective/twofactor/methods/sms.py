80a1064676cb4903cb5c78f76ed8d98877751792
Franco Pellegrini <frapell@gmail.com>
2013-12-07 22:02:48 +0100

    Cosmetic changes

--- src/collective/twofactor/methods/sms.py
+++ src/collective/twofactor/methods/sms.py
@@ -7,12 +7,12 @@ from plone.registry.interfaces import IRegistry
 from zope.browserpage import ViewPageTemplateFile
 from zope.component import getUtility
 from twilio.rest import TwilioRestClient
- 
+
 
 class SMSAuthentication(LocalAuthentication):
-    
+
     name = _("Authentication over SMS")
-    
+
     valid_already_sent = _(u"A code has already been sent to your phone.")
     new_code_sent = _(u"A new code has been generated and sent to your phone.")
 
@@ -21,11 +21,11 @@ class SMSAuthentication(LocalAuthentication):
         settings = registry.forInterface(ITwilioSettings)
 
         client = TwilioRestClient(settings.account_sid, settings.auth_token)
-        
+
         user_phone = self.member.getProperty('cell_phone', None)
         message = _("Use this code to authenticate: %s" % self.get_code())
 
-        client.messages.create(to=user_phone,
-                               from_=settings.phone_number,
-                               body=message)
+        client.sms.messages.create(to=user_phone,
+                                   from_=settings.phone_number,
+                                   body=message)
         super(SMSAuthentication, self).send_code()
